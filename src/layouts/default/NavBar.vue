1c6792da2f740eb66dc5e384f7d492559ed105e8
Luan Mello <close8821@gmail.com>
2024-03-14 22:30:03 -0300

    Feat | Atualizando Responsividade

--- src/layouts/default/NavBar.vue
+++ src/layouts/default/NavBar.vue
@@ -1,27 +1,15 @@
 <template>
-  <v-app-bar height="70" class="bg-white">
+  <v-app-bar height="74" class="bg-white">
 
     <v-row class="align-center d-flex justify-space-between">
-      <v-col cols="2" md="4" lg="4" class="d-none d-lg-block align-right pl-10">
-        <v-btn href="#linhasais" color="sentenary" class="font font-weight-bold rounded-pill bg-white"
-          style="border-width: 2px; border-color: #E457AF;">Linha Sais</v-btn>
-
-        <v-btn href="#linhaliquidos" color="sentenary" class="font font-weight-bold rounded-pill mx-8 bg-white"
-          style="border-width: 2px; border-color: #E457AF;">Linha Liquidos</v-btn>
-
-        <v-btn href="#oproduto" color="sentenary" class="font font-weight-bold rounded-pill bg-white"
-          style="border-width: 2px; border-color: #E457AF;">O
-          PRODUTO</v-btn>
-      </v-col>
-
-      <v-col cols="4" sm="5" class="pl-5 d-lg-none">
+      <v-col cols="4" sm="3" md="2" class="pl-5">
         <v-menu>
           <template v-slot:activator="{ props }">
             <v-btn color="sentenary" v-bind="props" icon="mdi-menu">
             </v-btn>
           </template>
           <v-list>
-            <v-list-item :href="item.click" @click="redirecionarPara(item)" v-for="(item, index) in itemsMobile"
+            <v-list-item class="text-sentenary" :href="item.click" @click="redirecionarPara(item)" v-for="(item, index) in itemsMobile"
               :key="index" :value="index">
               <v-list-item-title>{{ item.title }}</v-list-item-title>
             </v-list-item>
@@ -29,15 +17,15 @@
         </v-menu>
       </v-col>
 
-      <v-col cols="4" sm="" lg="5" >
+      <v-col cols="4" sm="3" md="2">
         <div class="d-flex">
-          <v-img src="/logohydro.png" max-width="68" :class="smAndDown ? 'mr-2' : 'ml-12 mr-5'" />
-          <v-img src="/logoleudo.png" max-width="68" />
+          <v-img src="/logohydro.png" min-width="68" max-width="68" :class="smAndDown ? 'mr-2' : 'ml-12 mr-5'" />
+          <v-img src="/logoleudo.png" min-width="68" max-width="68" />
         </div>
       </v-col>
      
 
-      <v-col cols="4" sm="2" lg="1">
+      <v-col cols="4" sm="3" md="2" align="right" class="pr-5">
         <v-dialog width="500">
           <template v-slot:activator="{ props }">
             <v-btn icon="mdi-account-circle" color="sentenary" v-bind="props"></v-btn>
@@ -48,22 +36,22 @@
                 <v-form v-model="valid">
                   <v-container>
                     <v-row>
-                      <v-col cols="12" md="10">
+                      <v-col cols="12" md="11">
                         <v-text-field v-model="nome" :rules="nomeRules" label="Nome" color="secondary"
                           hint="Informe seu nome" prepend-icon="mdi-account-circle-outline"></v-text-field>
                       </v-col>
 
-                      <v-col cols="12" md="10">
+                      <v-col cols="12" md="11">
                         <v-text-field v-model="Email" :rules="emailRules" label="Email" placeholder="email@email.com.br"
                           hint="Informe seu E-mail" color="green" prepend-icon="mdi-email"></v-text-field>
                       </v-col>
 
-                      <v-col cols="12" md="10">
+                      <v-col cols="12" md="11">
                         <v-text-field v-model="Senha" :rules="senhaRules" label="Senha" color="secondary"
                           hint="Informe sua senha" prepend-icon="mdi-form-textbox-password"></v-text-field>
                       </v-col>
 
-                      <v-col cols="12" md="10">
+                      <v-col cols="12" md="11">
                         <v-text-field v-model="RepitirSenha" :rules="repitirsenhaRules" label="Repitir Senha"
                           color="secondary" hint="Informe sua senha novamente"
                           prepend-icon="mdi-form-textbox-password"></v-text-field>
